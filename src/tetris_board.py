6e0439464e01122275ebab5f9f6f6c3552daf02d
Pro <wongpakornchokthitirath@gmail.com>
2024-04-22 23:16:24 +0700

    added guiding outlines for tetris pieces and cleaned up code a bit

--- src/tetris_board.py
+++ src/tetris_board.py
@@ -194,6 +194,15 @@ class TetrisBoard:
                 if cell == 'O':
                     pygame.draw.rect(screen, self.current_piece.color, ((self.current_piece.x + j) * GRID_SIZE, (self.current_piece.y + i) * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
 
+        # Draw outline
+        outline : Tetromino = Tetromino(self.current_piece.x, self.current_piece.y, self.current_piece.shape)
+        outline.rotation = self.current_piece.rotation
+        while (self.valid_move(outline, 0, 1, 0)):
+            outline.y += 1
+        for i, row in enumerate(outline.shape[outline.rotation % len(outline.shape)]):
+            for j, cell in enumerate(row):
+                if cell == 'O':
+                    pygame.draw.rect(screen, self.current_piece.color, ((outline.x + j) * GRID_SIZE, (outline.y + i) * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1), width = 2)
 
 class LiteTetrisBoard(TetrisBoard):
     """
@@ -202,41 +211,7 @@ class LiteTetrisBoard(TetrisBoard):
     """
     def __init__(self, width, height):
         super().__init__(width, height)
-        
 
-    def draw_rectangle(self, screen, x = None, y = GAME_OVER_HEIGHT * GRID_SIZE, width = None, height = None, color = RED):
-        if x is None:
-            x = self.width * GRID_SIZE
-        if width is None:
-            width = WIDTH - self.width
-        if height is None:
-            height = HEIGHT - self.height
-        
-
-        pygame.draw.rect(screen, color, (x,y,width, height))
-    
-    def draw(self, screen):
-        """
-        Draws the current game state to the screen, including the grid,
-        the current piece, and the game over height line.
-        """
-        screen.fill(BLACK)
-        self.draw_game_over_height(screen)
-        
-        # Draw each cell in the grid
-        for y, row in enumerate(self.grid):
-            for x, cell in enumerate(row):
-                if cell:
-                    pygame.draw.rect(screen, cell, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
-        
-        # Draw the current Tetromino
-        for i, row in enumerate(self.current_piece.shape[self.current_piece.rotation % len(self.current_piece.shape)]):
-            for j, cell in enumerate(row):
-                if cell == 'O':
-                    pygame.draw.rect(screen, self.current_piece.color, ((self.current_piece.x + j) * GRID_SIZE, (self.current_piece.y + i) * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
-        
-
-        self.draw_rectangle(screen)
 
 
 class RegularTetrisBoard(TetrisBoard):
@@ -246,38 +221,3 @@ class RegularTetrisBoard(TetrisBoard):
     """
     def __init__(self, width, height):
         super().__init__(width, height)
-        
-
-    def draw_rectangle(self, screen, x = None, y = GAME_OVER_HEIGHT * GRID_SIZE, width = None, height = None, color = RED):
-        if x is None:
-            x = self.width * GRID_SIZE
-        if width is None:
-            width = WIDTH - self.width
-        if height is None:
-            height = HEIGHT - self.height
-        
-
-        pygame.draw.rect(screen, color, (x,y,width, height))
-    
-    def draw(self, screen):
-        """
-        Draws the current game state to the screen, including the grid,
-        the current piece, and the game over height line.
-        """
-        screen.fill(BLACK)
-        self.draw_game_over_height(screen)
-        
-        # Draw each cell in the grid
-        for y, row in enumerate(self.grid):
-            for x, cell in enumerate(row):
-                if cell:
-                    pygame.draw.rect(screen, cell, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
-        
-        # Draw the current Tetromino
-        for i, row in enumerate(self.current_piece.shape[self.current_piece.rotation % len(self.current_piece.shape)]):
-            for j, cell in enumerate(row):
-                if cell == 'O':
-                    pygame.draw.rect(screen, self.current_piece.color, ((self.current_piece.x + j) * GRID_SIZE, (self.current_piece.y + i) * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
-        
-
-        self.draw_rectangle(screen)
